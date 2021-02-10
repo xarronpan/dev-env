f35dd551591fa4f4f6af816d9f3da80a2dee028c
panxiangrong <panxiangrong@yy.com>
2021-02-10 12:38:15 -0800

    fix fzf complete bug

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -2,4 +2,4 @@
 sudo apt-get install zsh
 mkdir -p ~/.zsh/antigen
 curl -L git.io/antigen > ~/.zsh/antigen/antigen.zsh
-git clone https://github.com/xarronpan/fzf-tab-completion .fzf-tab-completion
+git clone https://github.com/xarronpan/fzf-tab-completion ~/.fzf-tab-completion
