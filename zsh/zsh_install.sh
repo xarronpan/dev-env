8e0d1e89bedcef8afb5bb0b6f13d57589e7df6e1
panxiangrong <panxiangrong@yy.com>
2021-02-10 12:46:49 -0800

    add fzf install, tldr install

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 sudo apt-get install zsh
+
 mkdir -p ~/.zsh/antigen
 curl -L git.io/antigen > ~/.zsh/antigen/antigen.zsh
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 git clone https://github.com/xarronpan/fzf-tab-completion ~/.fzf-tab-completion
+
+mkdir -p ~/bin
+curl -o ~/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
+chmod +x ~/bin/tldr
