40a1f68a8b09c876358ca5dadb7f1516ac853343
panxiangrong <panxiangrong@yy.com>
2021-02-26 08:11:01 -0800

    增加tmux-pet插件，用于对复杂的命令进行记录snappet

--- packages/tmux.sh
+++ packages/tmux.sh
@@ -5,3 +5,6 @@ sudo apt install tmux-xpanes
 sudo apt install gawk
 sudo gem install tmuxinator
 git clone --depth 1 https://github.com/woodstok/tmux-butler ~/.tmux-butler
+mkdir -p ~/tmp && cd ~/tmp
+wget https://github.com/knqyf263/pet/releases/download/v0.3.0/pet_0.3.0_linux_amd64.deb
+sudo dpkg -i pet_0.3.0_linux_amd64.deb
