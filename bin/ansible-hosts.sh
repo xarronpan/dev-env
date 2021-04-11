8e28f847777222206e10b8b05c8c5f3081bcaf91
panxiangrong <panxiangrong@yy.com>
2021-04-11 20:22:43 +0800

    ansible-hosts脚本更加健壮

--- bin/ansible-hosts.sh
+++ bin/ansible-hosts.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 ENV=`ls ~/.inventory|fzf`
+if [ -z "$ENV" ]; then
+  exit 0
+fi
 GROUP_IDS=`ansible-inventory -i ~/.inventory/$ENV --list| \
   jq -r '.|keys| join(" ")'| \
   xargs -n 1| \
   grep -v 'all\|_meta'| \
   fzf -m`
-
+if [ -z "$GROUP_IDS" ]; then
+  exit 0
+fi
 for GROUP_ID in $GROUP_IDS; do
   ansible-inventory -i ~/.inventory/$ENV --list| \
   jq -r '.["'$GROUP_ID'"]|.hosts|join("\n")'
