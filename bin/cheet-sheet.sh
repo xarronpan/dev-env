20969197445f92b65f67aa65312805acd1ed7ce5
panxiangrong <panxiangrong@yy.com>
2021-04-12 17:02:03 +0800

    cheetsheet 显示的信息中不包括行号等信息

--- bin/cheet-sheet.sh
+++ bin/cheet-sheet.sh
@@ -16,21 +16,21 @@ case $SECTION in
   cht )
     cht.sh --shell;;
   bash )
-    bat ~/cheatsheets/languages/bash.sh ;;
+    bat --style=plain ~/cheatsheets/languages/bash.sh ;;
   golang )
-    bat ~/cheatsheets/languages/golang.go ;;
+    bat --style=plain ~/cheatsheets/languages/golang.go ;;
   python )
     glow -p ~/cheatsheets/languages/python.md ;;
   vim-script )
     glow -p ~/cheatsheets/languages/vimscript.md ;;
   gdb )
-    bat ~/cheatsheets/tools/gdb.txt ;;
+    bat --style=plain ~/cheatsheets/tools/gdb.txt ;;
   git )
-    bat ~/cheatsheets/tools/git.txt ;;
+    bat --style=plain ~/cheatsheets/tools/git.txt ;;
   tmux )
-    bat ~/cheatsheets/tools/tmux.txt ;;
+    bat --style=plain ~/cheatsheets/tools/tmux.txt ;;
   vim )
-    bat ~/cheatsheets/editors/vim.txt ;;
+    bat --style=plain ~/cheatsheets/editors/vim.txt ;;
   *)
     exit0 ;;
 esac
