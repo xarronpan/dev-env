cb980ebd5736fe5bbe6bd6c74f03f66dbb66a838
panxiangrong <panxiangrong@agora.io>
2021-06-15 09:40:11 +0800

    增加helm安装脚本

--- packages/k8s.sh
+++ packages/k8s.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+mkdir ~/tmp
+cd ~/tmp
+wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.6.0-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
