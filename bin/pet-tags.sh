6a7a09b52a2988896f6a80860fa5e03f8c938d51
panxiangrong <panxiangrong@agora.io>
2023-09-18 20:09:33 +0800

    feat: 调整pet展现信息

--- bin/pet-tags.sh
+++ bin/pet-tags.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-HEADER='types: shell vim docker k8s gdb helm'
+HEADER='shell vim docker k8s gdb helm git [. : @]'
 TAG=$(pet list|grep -oP '(?<=Tag: ).+'|sed 's/ /\n/g'|sort|uniq|sort -V -r|fzf --header="$HEADER" +s)
 if [ -z $TAG ]; then
   exit 0
