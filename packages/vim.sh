beca9b66d324fa07016f1efb8af5b60083401164
panxiangrong <panxiangrong@agora.io>
2023-09-26 23:15:29 +0800

    feat: 支持doge插件用于生成文档

--- packages/vim.sh
+++ packages/vim.sh
@@ -1,15 +1,6 @@
 sudo apt install silversearcher-ag
 sudo apt install universal-ctags
 
-sudo apt install build-essential cmake vim-nox python3-dev
-sudo apt install mono-complete golang nodejs default-jdk npm
-
-cd ~/.vim/bundle/YouCompleteMe
-sudo python3 install.py --all
-
-#对airline有一些修改，但是由于成本原因没有做成一个独立的插件, 已覆盖的方式进行配置
-cp vim-airline-themes/solarized_flood.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes 
-
 #下面两项工具用于vim的ycm插件生成clang编译数据库使用
 sudo pip install compiledb
 sudo pip install compdb
@@ -17,3 +8,16 @@ sudo pip install cpplint
 sudo pip install flake8
 
 sudo apt install clang-format
+
+#对airline有一些修改，但是由于成本原因没有做成一个独立的插件, 已覆盖的方式进行配置
+cp vim-airline-themes/solarized_flood.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes 
+
+#ycm
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install mono-complete golang nodejs default-jdk npm
+
+cd ~/.vim/bundle/YouCompleteMe
+sudo python3 install.py --all
+
+cd ~/.vim/bundle/vim-doge/scripts
+./install.sh
