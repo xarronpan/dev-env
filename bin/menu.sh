864bfbbb7230a0b0fcea99df1d006d7158efcf6b
panxiangrong <panxiangrong@agora.io>
2023-12-29 19:14:32 +0800

    feat: ranger自动补全功能放置到menu.sh进行触发

--- bin/menu.sh
+++ bin/menu.sh
@@ -3,6 +3,7 @@
 SECTION=$(fzf --prompt='sections>' --header="select cheet sheet sections:" <<MENU_SECTIONS
 ansible-hosts
 ansible-groups
+ranger
 MENU_SECTIONS
 )
 case $SECTION in 
@@ -10,6 +11,8 @@ case $SECTION in
     tmux split-window -v $HOME/bin/ansible-hosts-tmux.sh $1;;
   ansible-groups )
     tmux split-window -v $HOME/bin/ansible-groups-tmux.sh $1;;
+  ranger )
+    tmux split-window -v -c $2 $HOME/bin/ranger.sh $1;;
   *)
     exit 0;;
 esac
