2573da4c149ec234695543e61bfb67d75a4cde90
panxiangrong <panxiangrong@agora.io>
2024-01-02 14:19:48 +0800

    feat: edit completion dic功能，通过统一的menu入口触发

--- bin/menu.sh
+++ bin/menu.sh
@@ -4,6 +4,7 @@ SECTION=$(fzf --prompt='sections>' --header="select cheet sheet sections:" <<MEN
 ansible-hosts
 ansible-groups
 ranger
+edit-completion-dictionay
 MENU_SECTIONS
 )
 case $SECTION in 
@@ -13,6 +14,8 @@ case $SECTION in
     tmux split-window -v $HOME/bin/ansible-groups-tmux.sh $1;;
   ranger )
     tmux split-window -v -c $2 $HOME/bin/ranger.sh $1;;
+  edit-completion-dictionay )
+    tmux split-window -v vim $HOME/.compl_dic;;
   *)
     exit 0;;
 esac
