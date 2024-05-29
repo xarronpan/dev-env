2d9f49ae602ce00c188dc39d4cf397d8b78425db
panxiangrong <panxiangrong@agora.io>
2024-05-29 20:16:42 +0800

    支持更多的信息补全方式

--- bin/ansible-hosts-tmux.sh
+++ bin/ansible-hosts-tmux.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-tmux split-window $options tmux\ send-keys\ \-t\ $1\ \"\$\($HOME/bin/ansible-hosts.sh\)\"
+tmux split-window -v -l100% tmux\ send-keys\ \-t\ $1\ \"\$\($HOME/bin/ansible-hosts.sh\)\"
--- bin/menu.sh
+++ bin/menu.sh
@@ -3,6 +3,8 @@
 SECTION=$(fzf --prompt='sections>' --header="select cheet sheet sections:" <<MENU_SECTIONS
 ansible-hosts
 ansible-groups
+rtm-prod-hosts
+rtm-staging-hosts
 ranger
 edit-completion-dictionay
 MENU_SECTIONS
@@ -12,6 +14,10 @@ case $SECTION in
     tmux split-window -v $HOME/bin/ansible-hosts-tmux.sh $1;;
   ansible-groups )
     tmux split-window -v $HOME/bin/ansible-groups-tmux.sh $1;;
+  rtm-prod-hosts )
+    tmux split-window -v $HOME/bin/rtm-host-prod-tmux.sh $1;;
+  rtm-staging-hosts )
+    tmux split-window -v $HOME/bin/rtm-host-staging-tmux.sh $1;;
   ranger )
     tmux split-window -v -c $2 $HOME/bin/ranger.sh $1;;
   edit-completion-dictionay )
